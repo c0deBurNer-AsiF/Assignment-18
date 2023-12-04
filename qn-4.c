f2d849c53416caeacf8b48c1d6b14944af8b41f0
c0deBurNer-AsiF <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2023-12-05 00:11:05 +0530

    Create qn-4.c

---
+++ qn-4.c
@@ -0,0 +1,29 @@
+//4. Write a program to convert a given string into uppercase
+
+
+#include<stdio.h>
+void uppercase(char []);
+int main()
+{
+    char x[100];
+    uppercase(x);
+
+}
+void uppercase(char str[])
+{
+    int i;
+    printf("enter words or sentences:");
+    fgets(str,100,stdin);
+
+    for(i=0;str[i];i++)
+    {
+        if(str[i]>='a'&&str[i]<='z')
+        {
+            str[i]-=32;
+
+        }
+
+    }
+    printf("%s",str);
+
+}
