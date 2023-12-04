522ac0330fa54fb36c8ae3c45d49bc61d99b5137
c0deBurNer-AsiF <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2023-12-05 00:12:59 +0530

    Create qn-5.c

---
+++ qn-5.c
@@ -0,0 +1,29 @@
+//5. Write a program to convert a given string into lowercase
+
+
+#include<stdio.h>
+void uppercase(char []);
+int main()
+{
+    char x[100];
+    uppercase(x);
+
+}
+void uppercase(char str[])
+{
+    int i;
+    printf("enter words or sentences:");
+    fgets(str,100,stdin);
+
+    for(i=0;str[i];i++)
+    {
+        if(str[i]>='A'&&str[i]<='Z')
+        {
+            str[i]+=32;
+
+        }
+
+    }
+    printf("%s",str);
+
+}
